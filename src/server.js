c38b5b5fbfb3091a3ec23ecdf281ce20c4af9f6f
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2023-03-20 03:40:04 +0900

    #9 Authorization Header に API Token と User JWT 両方を設定可能に変更

--- src/server.js
+++ src/server.js
@@ -4,11 +4,15 @@ import axiosRetry from 'axios-retry';
 export default class Strapi {
   #_url;
   #_apiToken;
+  #_userJWT;
   #axiosInstance;
+  #axiosInstanceWithAPIToken;
+  #axiosInstanceWithUserJWT;
 
   constructor(config = {}) {
     this.#_url = config.url || process.env.STRAPI_URL || 'https://cms.ha-rehare.fans/';
     this.#_apiToken = config.apiToken || process.env.STRAPI_API_TOKEN || '';
+    this.#_userJWT = config.userJWT || null;
 
     this.#init();
   }
@@ -19,24 +23,71 @@ export default class Strapi {
   }
   set url(value) {
     this.#_url = value;
-    this.#init();
+    this.#initAxiosInstanceWithAPIToken();
+    this.#initAxiosInstanceWithUserJWT();
   }
 
   set apiToken(value) {
     this.#_apiToken = value;
-    this.#init();
+    this.#initAxiosInstanceWithAPIToken();
+  }
+
+  set userJWT(value) {
+    this.#_userJWT = value;
+    this.#initAxiosInstanceWithUserJWT();
   }
 
   // Utilities
   #init() {
+    this.#initAxiosInstance();
+    this.#initAxiosInstanceWithAPIToken();
+    this.#initAxiosInstanceWithUserJWT();
+  }
+
+  #initAxiosInstance() {
     this.#axiosInstance = axios.create({
+      baseURL: this.#_url,
+    });
+
+    axiosRetry(this.#axiosInstance, {
+      retries: 3,
+      retryDelay: axiosRetry.exponentialDelay,
+      onRetry: (retryCount, error, requestConfig) => {
+        if(retryCount === 1) return;
+        console.log(`[AxiosRetry] Retrying axios request \`${requestConfig.method.toUpperCase()} ${(new URL(requestConfig.baseURL, requestConfig.url)).toString()}\` due to an error "${error.message}" (${retryCount - 1})...`);
+        return;
+      },
+    });
+  }
+
+  #initAxiosInstanceWithAPIToken() {
+    this.#axiosInstanceWithAPIToken = axios.create({
       baseURL: this.#_url,
       headers: {
         Authorization: `Bearer ${this.#_apiToken}`,
       },
     });
 
-    axiosRetry(this.#axiosInstance, {
+    axiosRetry(this.#axiosInstanceWithAPIToken, {
+      retries: 3,
+      retryDelay: axiosRetry.exponentialDelay,
+      onRetry: (retryCount, error, requestConfig) => {
+        if(retryCount === 1) return;
+        console.log(`[AxiosRetry] Retrying axios request \`${requestConfig.method.toUpperCase()} ${(new URL(requestConfig.baseURL, requestConfig.url)).toString()}\` due to an error "${error.message}" (${retryCount - 1})...`);
+        return;
+      },
+    });
+  }
+
+  #initAxiosInstanceWithUserJWT() {
+    this.#axiosInstanceWithUserJWT = axios.create({
+      baseURL: this.#_url,
+      headers: {
+        Authorization: `Bearer ${this.#_userJWT}`,
+      },
+    });
+
+    axiosRetry(this.#axiosInstanceWithUserJWT, {
       retries: 3,
       retryDelay: axiosRetry.exponentialDelay,
       onRetry: (retryCount, error, requestConfig) => {
@@ -55,14 +106,32 @@ export default class Strapi {
       params: {
         access_token: accessToken,
       },
-    }).then(resp => {
-      return resp.data;
+    }).then(authResp => {
+      const {
+        jwt,
+      } = authResp.data;
+      this.userJWT = jwt;
+
+      return this.#axiosInstanceWithUserJWT.request({
+        method: 'get',
+        url: '/api/users/me',
+        params: {
+          populate: '*',
+        },
+      }).then(profileResp => {
+        const profile = profileResp.data;
+
+        return {
+          jwt,
+          user: profile,
+        };
+      });
     });
   }
 
   // API wrappers
   getProvidedServices(page = 1) {
-    return this.#axiosInstance.request({
+    return this.#axiosInstanceWithAPIToken.request({
       method: 'get',
       url: '/api/provided-services',
       params: {
@@ -81,7 +150,7 @@ export default class Strapi {
   }
 
   getRelatedLinks(key) {
-    return this.#axiosInstance.request({
+    return this.#axiosInstanceWithAPIToken.request({
       method: 'get',
       url: '/api/related-links-sets',
       params: {
